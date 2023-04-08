893bf9c754b0dc22fe5ecf59c1b793dce6a15c5d
car1803 <55598627+car1803@users.noreply.github.com>
2023-04-08 13:08:15 -0500

    endpoint sigin

--- routes/account/signIn/index.js
+++ routes/account/signIn/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
-
+var bcryptjs = require('bcryptjs');
+const pool = require('../../../database');
 /* GET home page. */
 router.get('/singIn', function(req, res, next) {
-  res.render('index', { title: 'singIn' });
+  res.render('signIn');
+});
+
+router.post('/singIn', async (req, res, next)=> {
+  const nombre = req.body.nombre;
+  const password = req.body.contrasena;
+  const correo = req.body.correo;
+  const rol = req.body.rol;
+  let passwordHash = await bcryptjs.hash(password, 10);
+  pool.query('INSERT INTO usuarios (nombre, password, correo, rol) VALUES ($1, $2, $3, $4)', [nombre, passwordHash, correo, rol], (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send('registro exitoso');
+  });
 });
 
 module.exports = router;
