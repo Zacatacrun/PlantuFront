e85762a4c9211d4075ead70378164fc535aee005
Kevin Fabio Ramos L <kramosl@unal.edu.co>
2023-04-08 00:39:32 -0500

    :sparkles: endpoint bulletin unsubscribe terminado

--- routes/bulletin/unsubscribe/index.js
+++ routes/bulletin/unsubscribe/index.js
@@ -1,10 +1,64 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const pool = require('../../../database');
+const validator = require('validator');
 
-/* GET home page. */
-router.get('/unsubscribe', function(req, res, next) {
-  res.render('index', { title: 'Unsubscribe' });
+// Endpoint para desuscribirse del boletín
+router.delete('/unsubscribe', (req, res) => {
+  // Obtener el correo electrónico del cuerpo de la solicitud
+  const email = req.body.email;
+
+  // Validar el correo electrónico
+  if (!validator.isEmail(email)) {
+    return res.json({
+      status: 0,
+      data: [],
+      warnings: [],
+      info: 'Error, correo electrónico inválido'
+    });
+  }
+
+  // Verificar si el correo electrónico está registrado en la tabla "bulletin"
+  pool.query('SELECT * FROM bulletin WHERE correo = ?', [email], (error, results) => {
+    if (error) {
+      return res.json({
+        status: 0,
+        data: [],
+        warnings: [],
+        info: 'Error interno, intentalo de nuevo'
+      });
+    }
+
+    // Si el correo electrónico no está registrado, devolver un error
+    if (results.length === 0) {
+      return res.json({
+        status: 0,
+        data: [],
+        warnings: [],
+        info: 'Error, el correo electrónico no está registrado'
+      });
+    }
+
+    // Eliminar el correo electrónico de la tabla "bulletin"
+    pool.query('DELETE FROM bulletin WHERE correo = ?', [email], (error, results) => {
+      if (error) {
+        return res.json({
+          status: 0,
+          data: [],
+          warnings: [],
+          info: 'Error interno, intentalo de nuevo'
+        });
+      }
+
+      // Devolver una respuesta exitosa
+      return res.json({
+        status: 1,
+        data: [],
+        warnings: [],
+        info: 'Solicitud para desuscribirte a nuestro boletín aprobada'
+      });
+    });
+  });
 });
-
 
 module.exports = router;
