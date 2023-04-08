29c40f33ec9270030095a95f5482518d7e642863
car1803 <55598627+car1803@users.noreply.github.com>
2023-04-08 14:22:08 -0500

    logout endpoint

--- routes/account/logout/index.js
+++ routes/account/logout/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var session = require('express-session');
 
 /* GET home page. */
 router.get('/logout', function(req, res, next) {
-  res.render('index', { title: 'logOut' });
+  req.session.destroy(()=>{
+    res.redirect('/');
+  });
 });
 
 module.exports = router;
