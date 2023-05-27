27946878fac0bcc43e8929aba6a3a6ca2c92128a
Kevin Fabio Ramos L <kramosl@unal.edu.co>
2023-05-27 12:11:00 -0500

    :sparkles: la suscripcion y desuscripcion del boletin de plantu funcionando con correos

--- routes/bulletin/subscribe/index.js
+++ routes/bulletin/subscribe/index.js
@@ -1,79 +1,103 @@
-var express = require('express');
-var router = express.Router();
-var pool = require('../../../database');
-var tokens = require('../../../tokens');
-var { isEmail } = require('validator');
+const express = require('express');
+const router = express.Router();
+const pool = require('../../../database');
+const tokens = require('../../../tokens');
+const { isEmail } = require('validator');
+const sendEmail = require('../../../sendEmail');
 
+router.post('/subscribe', async (req, res) => {
+  const token = await tokens.validateToken(pool, req.body.token);
+  if (!token) {
+    return res.status(401).json({
+      status: 0,
+      data: [],
+      warnings: ['No tienes permiso para realizar esta acción'],
+      info: 'Error interno, intentalo de nuevo',
+      token: req.body.token
+    });
+  }
 
-router.post('/subscribe' ,async (req, res) => {
-    var token = await tokens.validateToken(pool,req.body.token);
-    if (token) {
-        const email = req.body.email;
+  const email = req.body.email;
 
-        // Validar que el correo sea válido
-        if (!isEmail(email)) {
-            return res.status(400).json({
-                status: 0,
-                data: [],
-                warnings: ['El correo electrónico no es válido'],
-                info: 'Error interno, intentalo de nuevo',
-                token:req.body.token
-            });
-        }
+  // Validar que el correo sea válido
+  if (!isEmail(email)) {
+    return res.status(400).json({
+      status: 0,
+      data: [],
+      warnings: ['El correo electrónico no es válido'],
+      info: 'Error interno, intentalo de nuevo',
+      token: req.body.token
+    });
+  }
 
-        // Validar que el correo no esté ya registrado
-        pool.query('SELECT * FROM bulletin WHERE correo = ?', [email], (err, results) => {
-            if (err) {
-                return res.status(500).json({
-                    status: 0,
-                    data: [],
-                    warnings: ['Error interno en la base de datos'],
-                    info: 'Error interno, intentalo de nuevo',
-                    token:req.body.token
-                });
-            }
-            if (results.length > 0) {
-                return res.status(409).json({
-                    status: 0,
-                    data: [],
-                    warnings: [],
-                    info: 'ya estas suscrito a nuestro boletín',
-                    token:req.body.token
-                });
-            }
-
-            // Insertar el nuevo usuario en la base de datos
-            pool.query('INSERT INTO bulletin (correo) VALUES (?)', [email], (err, results) => {
-                if (err) {
-                    return res.status(500).json({
-                        status: 0,
-                        data: [],
-                        warnings: ['Error interno en la base de datos'],
-                        info: 'Error interno, intentalo de nuevo',
-                        token:req.body.token
-                        
-                    });
-                }
-
-                return res.status(201).json({
-                    status: 1,
-                    data: [],
-                    warnings: [],
-                    info: 'Gracias por suscribirte a nuestro boletín',
-                    token:req.body.token
-                });
-            });
-        });
+  // Validar que el correo no esté ya registrado
+  pool.query('SELECT * FROM bulletin WHERE correo = ?', [email], (err, results) => {
+    if (err) {
+      return res.status(500).json({
+        status: 0,
+        data: [],
+        warnings: ['Error interno en la base de datos'],
+        info: 'Error interno, intentalo de nuevo',
+        token: req.body.token
+      });
     }
-    else{
-        return res.status(401).json({
+    if (results.length > 0) {
+      return res.status(409).json({
+        status: 0,
+        data: [],
+        warnings: [],
+        info: 'ya estas suscrito a nuestro boletín',
+        token: req.body.token
+      });
+    }
+
+    // Insertar el nuevo usuario en la base de datos
+    pool.query('INSERT INTO bulletin (correo) VALUES (?)', [email], (err, results) => {
+      if (err) {
+        return res.status(500).json({
+          status: 0,
+          data: [],
+          warnings: ['Error interno en la base de datos'],
+          info: 'Error interno, intentalo de nuevo',
+          token: req.body.token
+        });
+      }
+
+      const emailBody = "Estimado/a cliente,<br><br>Gracias por suscribirse a plantu, la plataforma líder en venta de plantas online. Estamos encantados de contar con usted como parte de nuestra comunidad de amantes de la naturaleza.<br><br>En plantu encontrará una gran variedad de plantas de calidad, desde las más exóticas hasta las más clásicas, y podrá comprarlas con total seguridad y comodidad desde su casa. Además, podrá acceder a ofertas exclusivas, consejos de cuidado y envíos gratuitos.<br><br>Esperamos que disfrute de su experiencia en plantu y que encuentre las plantas que más le gusten. Si tiene alguna duda o sugerencia, no dude en contactarnos.<br><br>Atentamente,<br>El equipo de plantu";
+      const emailSubject = "¡Bienvenido/a a Plantu!";
+      const merror = 'Error al enviar el correo electrónico de confirmación';
+
+      sendEmail.SendEmail(email, emailSubject, emailBody, merror, res)
+        .then((emailSent) => {
+          if (!emailSent) {
+            return res.status(409).json({
+              status: 0,
+              data: [],
+              warnings: ["se ha producido un error al enviar el correo"],
+              info: 'ya estas suscrito a nuestro boletín',
+              token: req.body.token
+            });
+          } else {
+            return res.status(201).json({
+              status: 1,
+              data: [],
+              warnings: ["se ha enviado un correo de confirmación a tu correo electrónico"],
+              info: 'Gracias por suscribirte a nuestro boletín',
+              token: req.body.token
+            });
+          }
+        })
+        .catch((error) => {
+          return res.status(500).json({
             status: 0,
             data: [],
-            warnings: ['No tienes permiso para realizar esta acción'],
+            warnings: ['Error al enviar el correo'],
             info: 'Error interno, intentalo de nuevo',
-            token:req.body.token
+            token: req.body.token
+          });
         });
-    }
+    });
+  });
 });
 
 module.exports = router;
--- routes/bulletin/unsubscribe/index.js
+++ routes/bulletin/unsubscribe/index.js
@@ -1,15 +1,16 @@
-var express = require('express');
-var router = express.Router();
-var pool = require('../../../database');
-var validator = require('validator');
-var tokens = require('../../../tokens');
+const express = require('express');
+const router = express.Router();
+const pool = require('../../../database');
+const validator = require('validator');
+const tokens = require('../../../tokens');
+const sendEmail = require('../../../sendEmail');
 // Endpoint para desuscribirse del boletín
 router.delete('/unsubscribe', async(req, res) => {
-  var token= await tokens.validateToken(pool,req.body.token);
+  const token= await tokens.validateToken(pool,req.body.token);
   if (token)
   {
     // Obtener el correo electrónico del cuerpo de la solicitud
-    var email = req.body.email;
+    const email = req.body.email;
 
     // Validar el correo electrónico
     if (!validator.isEmail(email)) {
@@ -56,15 +57,43 @@ router.delete('/unsubscribe', async(req, res) => {
             token:req.body.token
           });
         }
+        const emailSubject = "Lamentamos que hayas cancelado tu suscripción a Plantu";
+        const emailBody = "Estimado/a cliente,<br><br>Lamentamos que hayas cancelado tu suscripción a Plantu, la plataforma líder en venta de plantas online. Apreciamos tu interés en nuestra comunidad de amantes de la naturaleza.<br><br>Recuerda que siempre puedes volver a suscribirte en cualquier momento para recibir nuestras ofertas exclusivas, consejos de cuidado y envíos gratuitos.<br><br>Si tienes alguna pregunta o necesitas asistencia, no dudes en contactarnos. Estaremos encantados de ayudarte.<br><br>Gracias por tu atención y esperamos verte nuevamente en Plantu en el futuro.<br><br>Atentamente,<br>El equipo de Plantu";
 
+
+        const merror = 'Error al enviar el correo electrónico de confirmación';
+  
+        sendEmail.SendEmail(email,emailSubject, emailBody, merror)
+          .then((emailSent) => {
+            if (!emailSent) {
+              return res.status(409).json({
+                status: 0,
+                data: [],
+                warnings: ["se ha producido un error al enviar el correo"],
+                info: 'Solicitud para desuscribirte a nuestro boletín aprobada',
+                token: req.body.token
+              });
+            } else {
+              return res.status(200).json({
+                status: 1,
+                data: [],
+                warnings: [],
+                info: 'Solicitud para desuscribirte a nuestro boletín aprobada',
+                token:req.body.token
+              });
+            }
+          })
+          .catch((error) => {
+            return res.status(500).json({
+              status: 0,
+              data: [],
+              warnings: ['Error al enviar el correo'],
+              info: 'Error interno, intentalo de nuevo',
+              token: req.body.token
+            });
+          });
         // Devolver una respuesta exitosa
-        return res.status(200).json({
-          status: 1,
-          data: [],
-          warnings: [],
-          info: 'Solicitud para desuscribirte a nuestro boletín aprobada',
-          token:req.body.token
-        });
+        
       });
     });
   }
