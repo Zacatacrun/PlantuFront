c10942f2fbbd663c95347eaeb0fb1b632284bae9
Carlos Jiménez <cajimenezca@unal.edu.co>
2023-05-31 18:50:38 -0500

    :sparkles: :bug: filldb

--- routes/fill/index.js
+++ routes/fill/index.js
@@ -6,17 +6,14 @@ router.get(
   "/9a033a567c2f1efe0645cdd8671ba91f34dcb410f250132e1e02d00675e21497",
   async (req, res) => {
     try {
-      //delete users
-      await pool.query("DELETE FROM usuarios");
-
-      //delete categories
-      await pool.query("DELETE FROM categorias");
-
-      //delete viveros
-      await pool.query("DELETE FROM viveros");
-
       //delete products
       await pool.query("DELETE FROM plantas");
+      //delete viveros
+      await pool.query("DELETE FROM viveros");
+      //delete categories
+      await pool.query("DELETE FROM categorias");
+      //delete users
+      await pool.query("DELETE FROM usuarios");
 
       // add users
       for (let i = 0; i < 100; i++) {
