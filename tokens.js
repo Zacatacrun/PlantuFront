67c538615dcc89f6d726af5e14e17c32ae7f3498
Carlos Jiménez <cajimenezca@unal.edu.co>
2023-06-04 15:14:27 -0500

    🐛 Login

--- tokens.js
+++ tokens.js
@@ -50,216 +50,288 @@
 ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣿⣾⣯⣿⣽⣾⣿⣿⣾⣯⣿⣟⣿⣻⣽⣺⣷⣻⣽⣟⣷⣿⣾⣷⣯⣿⣽⣿⣾⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
 ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ */
 
-//metodo que recibe un usuario y un token y lo guarda en la base de datos, retorna true si se guardo o false si no se guardo 
-async function saveToken(pool,user, token) {
-    try {
-      // Buscamos al usuario en la base de datos
-      const rows = await pool.query('SELECT * FROM usuarios WHERE correo = ? OR nombre = ?', [user, user]);
-      //convert the rows to an json object
-      const userObj = JSON.parse(JSON.stringify(rows));
-      // Validamos que el usuario exista
-
-      if (rows.length === 0) {
-        //throw new Error('El usuario no existe en la base de datos');
-        //imprime el error en la consola
-        console.log('El usuario no existe en la base de datos');
-        return false;
-      }
-      //verifica si el usuario tiene un token activo
-      const rows2 = await pool.query('SELECT * FROM tokens WHERE usuario_id = ?', [userObj[0].id]);
-      if (rows2.length !== 0) {
-        //throw new Error('El usuario ya tiene un token activo');
-        //imprime el error en la consola
-        console.log('El usuario ya tiene un token activo');
-        return true;
-      }
-      //get the id of the user from the json object
-      const userId = userObj[0].id;
-      const fecha = new Date();
-      const fecha2 = fecha.getFullYear() + '-' + (fecha.getMonth() + 1) + '-' + fecha.getDate() + ' ' + fecha.getHours() + ':' + fecha.getMinutes() + ':' + fecha.getSeconds();
-      const result = await pool.query('INSERT INTO tokens (token, usuario_id,fecha_creacion) VALUES (?, ?, ?)', [token, userId, fecha2]);
-      const tokenObj = JSON.parse(JSON.stringify(result));
-      // Retornamos verdadero si se insertó el token
-      return true;
-    } catch (error) {
-      console.log(error);
+//metodo que recibe un usuario y retorna el token de ese usuario o false si no tiene token
+async function getToken(pool, user) {
+  try {
+    // Buscamos al usuario en la base de datos
+    const rows = await pool.query(
+      "SELECT * FROM usuarios WHERE correo = ? OR nombre = ?",
+      [user, user]
+    );
+    //convert the rows to an json object
+    const userObj = JSON.parse(JSON.stringify(rows));
+    // Validamos que el usuario exista
+    if (rows.length === 0) {
+      //throw new Error('El usuario no existe en la base de datos');
+      //imprime el error en la consola
+      console.log("El usuario no existe en la base de datos");
       return false;
     }
-  }
-//metodo que recibe un token y valida si existe en la base de datos y si es valido, retorna true si es valido o false si no lo es
-async function validateToken(pool,token) {
-    try {
-       
-        // Buscamos el token en la base de datos
-        const rows = await pool.query('SELECT * FROM tokens WHERE token = ?', [token]);
-        //convert the rows to an json object
-        const tokenObj = JSON.parse(JSON.stringify(rows));
-        
-        // Validamos que el token exista
-        if (rows.length === 0) {
-            //throw new Error('El token no existe en la base de datos');
-            //imprime el error en la consola
-            console.log('El token no existe en la base de datos');
-            return false;
-        }
-        
-        //get the id of the token from the json object
-        const tokenId = tokenObj[0].id;
-        
-        //get the date of the token from the json object
-        const tokenDate = tokenObj[0].fecha_creacion;
-        
-        //get the date of today
-        const today = new Date();
-        
-        //get the date of the token
-        const tokenDateObj = new Date(tokenDate);
-        
-        //compare the dates
-        if (tokenDateObj > today) {
-            console.log('La fecha de creación del token es mayor a la fecha actual');
-            return false;
-        }
-        
-        //get the difference between the date of today and the date of the token
-        const diffTime = Math.abs(today - tokenDateObj);
-        
-        //get the difference in days
-        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-        //if the difference in days is greater than 1 delete the token from the database
-        if (diffDays > 1) {
-            //delete the token from the database
-            await pool.query('DELETE FROM tokens WHERE id = ?', [tokenId]);
-            return false;
-        }
-        
-
-        //if the difference in days is less than 1 return true
-        return true;
-    } catch (error) {
-        console.log(error);
-        return false;
+    //verifica si el usuario tiene un token activo
+    const rows2 = await pool.query(
+      "SELECT * FROM tokens WHERE usuario_id = ?",
+      [userObj[0].id]
+    );
+    if (rows2.length === 0) {
+      //throw new Error('El usuario no tiene un token activo');
+      //imprime el error en la consola
+      console.log("El usuario no tiene un token activo");
+      return false;
     }
+    //convert the rows to an json object
+    const tokenObj = JSON.parse(JSON.stringify(rows2));
+    //retorna el token del usuario
+    return tokenObj[0].token;
+  } catch (error) {
+    //imprime el error en la consola
+    console.log(error);
+    return false;
+  }
+}
+
+//metodo que recibe un usuario y un token y lo guarda en la base de datos, retorna true si se guardo o false si no se guardo
+async function saveToken(pool, user, token) {
+  try {
+    // Buscamos al usuario en la base de datos
+    const rows = await pool.query(
+      "SELECT * FROM usuarios WHERE correo = ? OR nombre = ?",
+      [user, user]
+    );
+    //convert the rows to an json object
+    const userObj = JSON.parse(JSON.stringify(rows));
+    // Validamos que el usuario exista
+
+    if (rows.length === 0) {
+      //throw new Error('El usuario no existe en la base de datos');
+      //imprime el error en la consola
+      console.log("El usuario no existe en la base de datos");
+      return false;
+    }
+    //verifica si el usuario tiene un token activo
+    const rows2 = await pool.query(
+      "SELECT * FROM tokens WHERE usuario_id = ?",
+      [userObj[0].id]
+    );
+    if (rows2.length !== 0) {
+      //throw new Error('El usuario ya tiene un token activo');
+      //imprime el error en la consola
+      console.log("El usuario ya tiene un token activo");
+      return true;
+    }
+    //get the id of the user from the json object
+    const userId = userObj[0].id;
+    const fecha = new Date();
+    const fecha2 =
+      fecha.getFullYear() +
+      "-" +
+      (fecha.getMonth() + 1) +
+      "-" +
+      fecha.getDate() +
+      " " +
+      fecha.getHours() +
+      ":" +
+      fecha.getMinutes() +
+      ":" +
+      fecha.getSeconds();
+    const result = await pool.query(
+      "INSERT INTO tokens (token, usuario_id,fecha_creacion) VALUES (?, ?, ?)",
+      [token, userId, fecha2]
+    );
+    const tokenObj = JSON.parse(JSON.stringify(result));
+    // Retornamos verdadero si se insertó el token
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+}
+//metodo que recibe un token y valida si existe en la base de datos y si es valido, retorna true si es valido o false si no lo es
+async function validateToken(pool, token) {
+  try {
+    // Buscamos el token en la base de datos
+    const rows = await pool.query("SELECT * FROM tokens WHERE token = ?", [
+      token,
+    ]);
+    //convert the rows to an json object
+    const tokenObj = JSON.parse(JSON.stringify(rows));
+
+    // Validamos que el token exista
+    if (rows.length === 0) {
+      //throw new Error('El token no existe en la base de datos');
+      //imprime el error en la consola
+      console.log("El token no existe en la base de datos");
+      return false;
+    }
+
+    //get the id of the token from the json object
+    const tokenId = tokenObj[0].id;
+
+    //get the date of the token from the json object
+    const tokenDate = tokenObj[0].fecha_creacion;
+
+    //get the date of today
+    const today = new Date();
+
+    //get the date of the token
+    const tokenDateObj = new Date(tokenDate);
+
+    //compare the dates
+    if (tokenDateObj > today) {
+      console.log("La fecha de creación del token es mayor a la fecha actual");
+      return false;
+    }
+
+    //get the difference between the date of today and the date of the token
+    const diffTime = Math.abs(today - tokenDateObj);
+
+    //get the difference in days
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    //if the difference in days is greater than 1 delete the token from the database
+    if (diffDays > 1) {
+      //delete the token from the database
+      await pool.query("DELETE FROM tokens WHERE id = ?", [tokenId]);
+      return false;
+    }
+
+    //if the difference in days is less than 1 return true
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 }
 //Este metodo debe llamarse al finalizar la sesion del usuario, recibe un token y lo elimina de la base de datos, retorna true si se elimino o false si no se elimino
-async function deleteToken(pool,token) {
-    try {
-        // Buscamos el token en la base de datos
-        const rows = await pool.query('SELECT * FROM tokens WHERE token = ?', [token]);
-        //convert the rows to an json object
-        const tokenObj = JSON.parse(JSON.stringify(rows));
-        // Validamos que el token exista
-        if (rows.length === 0) {
-            //throw new Error('El token no existe en la base de datos');
-            //imprime el error en la consola
-            console.log('El token no existe en la base de datos');
-            return false;
-        }
-        //get the id of the token from the json object
-        const tokenId = tokenObj[0].id;
-        //delete the token from the database
-        await pool.query('DELETE FROM tokens WHERE id = ?', [tokenId]);
-        return true;
-    } catch (error) {
-        console.log(error);
-        return false;
+async function deleteToken(pool, token) {
+  try {
+    // Buscamos el token en la base de datos
+    const rows = await pool.query("SELECT * FROM tokens WHERE token = ?", [
+      token,
+    ]);
+    //convert the rows to an json object
+    const tokenObj = JSON.parse(JSON.stringify(rows));
+    // Validamos que el token exista
+    if (rows.length === 0) {
+      //throw new Error('El token no existe en la base de datos');
+      //imprime el error en la consola
+      console.log("El token no existe en la base de datos");
+      return false;
     }
+    //get the id of the token from the json object
+    const tokenId = tokenObj[0].id;
+    //delete the token from the database
+    await pool.query("DELETE FROM tokens WHERE id = ?", [tokenId]);
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 }
 // este metodo recibe un token y valida si el usuario es admin, retorna true si es valido o false si no lo es
 async function validateAdminToken(pool, token) {
   try {
-      // Buscamos el token en la base de datos
-      const rows = await pool.query('SELECT * FROM tokens WHERE token = ?', [token]);
-      // Validamos que el token exista
-      if (rows.length === 0) {
-          console.log('El token no existe en la base de datos');
-          return false;
-      }
-      
-      const tokenId = rows[0].id;
-      const tokenDate = rows[0].fecha_creacion;
-      
-      const today = new Date();
-      const tokenDateObj = new Date(tokenDate);
-      
-      if (tokenDateObj > today) {
-          console.log('La fecha de creación del token es mayor a la fecha actual');
-          return false;
-      }
-      
-      const diffTime = Math.abs(today - tokenDateObj);
-      const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-      
-      if (diffDays > 1) {
-          await pool.query('DELETE FROM tokens WHERE id = ?', [tokenId]);
-          return false;
-      }
-
-      // Verificar si el usuario es un vivero (admin)
-      const user_id = rows[0].usuario_id;
-      const userRows = await pool.query('SELECT * FROM usuarios WHERE id = ? AND rol = ?', [user_id, 'admin']);
-      if (userRows.length === 0) {
-          console.log('El usuario no es un admin');
-          return false;
-      }
-      
-      // Si pasa todas las validaciones anteriores, el token es válido y el usuario es un vivero (admin)
-      return true;
-  } catch (error) {
-      console.log(error);
+    // Buscamos el token en la base de datos
+    const rows = await pool.query("SELECT * FROM tokens WHERE token = ?", [
+      token,
+    ]);
+    // Validamos que el token exista
+    if (rows.length === 0) {
+      console.log("El token no existe en la base de datos");
       return false;
+    }
+
+    const tokenId = rows[0].id;
+    const tokenDate = rows[0].fecha_creacion;
+
+    const today = new Date();
+    const tokenDateObj = new Date(tokenDate);
+
+    if (tokenDateObj > today) {
+      console.log("La fecha de creación del token es mayor a la fecha actual");
+      return false;
+    }
+
+    const diffTime = Math.abs(today - tokenDateObj);
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+    if (diffDays > 1) {
+      await pool.query("DELETE FROM tokens WHERE id = ?", [tokenId]);
+      return false;
+    }
+
+    // Verificar si el usuario es un vivero (admin)
+    const user_id = rows[0].usuario_id;
+    const userRows = await pool.query(
+      "SELECT * FROM usuarios WHERE id = ? AND rol = ?",
+      [user_id, "admin"]
+    );
+    if (userRows.length === 0) {
+      console.log("El usuario no es un admin");
+      return false;
+    }
+
+    // Si pasa todas las validaciones anteriores, el token es válido y el usuario es un vivero (admin)
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
   }
 }
 // este metodo recibe un token y valida si el usuario esta asociado a un vivero, retorna true si es valido o false si no lo es
 async function validateViveroToken(pool, token) {
-    try {
-        // Buscamos el token en la base de datos
-        const rows = await pool.query('SELECT * FROM tokens WHERE token = ?', [token]);
-        
-        // Validamos que el token exista
-        if (rows.length === 0) {
-            console.log('El token no existe en la base de datos');
-            return false;
-        }
-        
-        const tokenId = rows[0].id;
-        const tokenDate = rows[0].fecha_creacion;
-        
-        const today = new Date();
-        const tokenDateObj = new Date(tokenDate);
-        
-        if (tokenDateObj > today) {
-            console.log('La fecha de creación del token es mayor a la fecha actual');
-            return false;
-        }
-        
-        const diffTime = Math.abs(today - tokenDateObj);
-        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-        
-        if (diffDays > 1) {
-            await pool.query('DELETE FROM tokens WHERE id = ?', [tokenId]);
-            return false;
-        }
-  
-        // Verificar si el usuario es un vivero (admin)
-        const user_id = rows[0].usuario_id;
-        const userRows = await pool.query('SELECT * FROM usuarios WHERE id = (SELECT vendedor_id FROM viveros WHERE vendedor_id = ?)', [user_id]);
-        if (userRows.length === 0) {
-            console.log('El usuario no es un vivero ');
-            return false;
-        }
-        
-        // Si pasa todas las validaciones anteriores, el token es válido y el usuario es un vivero (admin)
-        return true;
-    } catch (error) {
-        console.log(error);
-        return false;
+  try {
+    // Buscamos el token en la base de datos
+    const rows = await pool.query("SELECT * FROM tokens WHERE token = ?", [
+      token,
+    ]);
+
+    // Validamos que el token exista
+    if (rows.length === 0) {
+      console.log("El token no existe en la base de datos");
+      return false;
     }
+
+    const tokenId = rows[0].id;
+    const tokenDate = rows[0].fecha_creacion;
+
+    const today = new Date();
+    const tokenDateObj = new Date(tokenDate);
+
+    if (tokenDateObj > today) {
+      console.log("La fecha de creación del token es mayor a la fecha actual");
+      return false;
+    }
+
+    const diffTime = Math.abs(today - tokenDateObj);
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+    if (diffDays > 1) {
+      await pool.query("DELETE FROM tokens WHERE id = ?", [tokenId]);
+      return false;
+    }
+
+    // Verificar si el usuario es un vivero (admin)
+    const user_id = rows[0].usuario_id;
+    const userRows = await pool.query(
+      "SELECT * FROM usuarios WHERE id = (SELECT vendedor_id FROM viveros WHERE vendedor_id = ?)",
+      [user_id]
+    );
+    if (userRows.length === 0) {
+      console.log("El usuario no es un vivero ");
+      return false;
+    }
+
+    // Si pasa todas las validaciones anteriores, el token es válido y el usuario es un vivero (admin)
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
   }
-  module.exports = {
-    saveToken,
-    validateToken,
-    deleteToken,
-    validateViveroToken,
-    validateAdminToken
-  };
+}
+module.exports = {
+  saveToken,
+  validateToken,
+  deleteToken,
+  validateViveroToken,
+  validateAdminToken,
+  getToken,
+};
